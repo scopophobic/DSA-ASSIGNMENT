8cb2123dabacc955c2e3d717bdc0178e5813d390
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-01-17 11:25:39 +0530

    Update find all duplicates in any array.cpp

--- Assignment hashing.cpp/find all duplicates in any array.cpp
+++ Assignment hashing.cpp/find all duplicates in any array.cpp
@@ -1,17 +1,15 @@
-                           
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int>duplicates;
             map<int,int>mp;
-            for(int i=1;i<nums.size();i++)
+            for(int i=0;i<nums.size();i++)
             {
                 mp[nums[i]]++;
             }
             for(auto i : mp)
             { 
-                 mp[i.first]++;
-               if(i.second==2)
+               if(i.second>1)
                {
                    duplicates.push_back(i.first);
                    
